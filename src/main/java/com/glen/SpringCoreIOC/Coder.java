b250a33611f4200977698a2837a5daef0f0dfb80
glen-s-abraham <glenprojects666@gmail.com>
2021-11-25 17:53:40 +0530

    Till bean scopes

---
+++ src/main/java/com/glen/SpringCoreIOC/Coder.java
@@ -0,0 +1,14 @@
+package com.glen.SpringCoreIOC;
+
+public class Coder {
+	
+	int age;
+	
+	public Coder() {
+		System.out.println("Coder Object created");
+	}
+
+	public void code() {
+		System.out.println("Coding....");
+	}
+}
